c926af46d78a6ecc72ee5ac458941e0b4d8df60d
99p <poccori@gmail.com>
2021-09-29 00:34:09 +0900

    Initial Commit

---
+++ SwiftUI_01/SwiftUI_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_01App.swift
+//  SwiftUI_01
+//
+//  Created by macboy on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
